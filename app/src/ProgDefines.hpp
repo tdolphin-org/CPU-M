ead0b1384ab9765b2e19140f489d6400ede48dd9
Rafal Zabdyr <rafal.zabdyr@tdolphin.org>
2025-08-25 21:54:30 +0200

    add initial project files including workspace, versioning, and definitions

---
+++ app/src/ProgDefines.hpp
@@ -0,0 +1,20 @@
+//
+//  CPU-M
+//
+//  (c) 2025 TDolphin
+//
+
+#pragma once
+
+#define AMIGAOS_VER_PREFIX "$VER:"
+
+#define AUTHORS "Rafal Zabdyr"
+
+#define CMD_NAME "CPU-M"
+#define APP_NAME "CPU-M"
+#define APP_DESCRIPTION "System Information Tool"
+#define APP_TDOLPHIN_COPYRIGHTS "(c) 2025 TDolphin"
+
+#define TDOLPHIN_WEBPAGE "http://www.tdolphin.org"
+#define TDOLPHIN_EMAIL "info@tdolphin.org"
+#define GIT_REPOSITORY_URL "https://github.com/tdolphin-org/CPU-M"
