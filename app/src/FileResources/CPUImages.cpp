8a4178d6d7b9828dc2dbaa1ba6f400deea8ec7f5
Rafal Zabdyr <rafal.zabdyr@tdolphin.org>
2025-08-27 18:42:23 +0200

    add CPU image resource files for various PPC models

---
+++ app/src/FileResources/CPUImages.cpp
@@ -0,0 +1,20 @@
+//
+//  CPU-M
+//
+//  (c) 2025 TDolphin
+//
+
+#include "CPUImages.hpp"
+
+namespace CPUImageFile
+{
+    const char *unknown = "PROGDIR:images/cpu/unknown.64x64.png";
+    const char *ppc = "PROGDIR:images/cpu/ppc.64x64.png";
+    const char *ppc602 = "PROGDIR:images/cpu/ppc602.64x64.png";
+    const char *ppc603 = "PROGDIR:images/cpu/ppc603.64x64.png";
+    const char *ppc603e = "PROGDIR:images/cpu/ppc603e.64x64.png";
+    const char *ppc603p = "PROGDIR:images/cpu/ppc603p.64x64.png";
+    const char *ppc604 = "PROGDIR:images/cpu/ppc604.64x64.png";
+    const char *ppc604e = "PROGDIR:images/cpu/ppc604e.64x64.png";
+    const char *ppc620 = "PROGDIR:images/cpu/ppc620.64x64.png";
+}
---
+++ app/src/FileResources/CPUImages.hpp
@@ -0,0 +1,20 @@
+//
+//  CPU-M
+//
+//  (c) 2025 TDolphin
+//
+
+#pragma once
+
+namespace CPUImageFile
+{
+    extern const char *unknown;
+    extern const char *ppc;
+    extern const char *ppc602;
+    extern const char *ppc603;
+    extern const char *ppc603e;
+    extern const char *ppc603p;
+    extern const char *ppc604;
+    extern const char *ppc604e;
+    extern const char *ppc620;
+}
