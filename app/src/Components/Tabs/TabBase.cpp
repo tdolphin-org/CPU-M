06cf63838fca9d8a96a156f15a9ebb4d7652c54d
Rafal Zabdyr <rafal.zabdyr@tdolphin.org>
2025-08-27 19:43:16 +0200

    add TabBase class implementation

---
+++ app/src/Components/Tabs/TabBase.cpp
@@ -0,0 +1,24 @@
+//
+//  CPU-M
+//
+//  (c) 2025 TDolphin
+//
+
+#include "TabBase.hpp"
+
+namespace Components
+{
+    MUI::Text TabBase::LabelText(const std::string &label)
+    {
+        return MUI::TextBuilder().tagContents(label).tagFrame(MUI::Frame::String).tagPhantomHoriz(true).tagWeight(0).object();
+    }
+
+    MUI::Text TabBase::ValueText(const std::string &help, const std::string &defaultValue)
+    {
+        return MUI::TextBuilder()
+            .tagFrame(MUI::Frame::String)
+            .tagContents(defaultValue)
+            .tagShortHelp(help)
+            .object();
+    }
+}
---
+++ app/src/Components/Tabs/TabBase.hpp
@@ -0,0 +1,19 @@
+//
+//  CPU-M
+//
+//  (c) 2025 TDolphin
+//
+
+#pragma once
+
+#include "MUI/Text.hpp"
+
+namespace Components
+{
+    class TabBase
+    {
+      public:
+        MUI::Text LabelText(const std::string &label);
+        MUI::Text ValueText(const std::string &help, const std::string &defaultValue = "--");
+    };
+}
