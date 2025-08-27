0546c8bc0fd4571bc6d3654d7ca511a391828c24
Rafal Zabdyr <rafal.zabdyr@tdolphin.org>
2025-08-27 19:23:28 +0200

    add AppContextCore class implementation with methods for OpenURLBase and CybergraphicsBase scopes

---
+++ app/src/AppContext.cpp
@@ -0,0 +1,37 @@
+//
+//  CPU-M
+//
+//  (c) 2025 TDolphin
+//
+
+#include "AppContext.hpp"
+
+#include "App.hpp"
+
+#ifdef TRACE
+#include <iostream.hpp>
+#endif
+
+AppContextCore::AppContextCore()
+{
+#ifdef TRACE
+    std::cout << __PRETTY_FUNCTION__ << std::endl;
+#endif
+}
+
+AppContextCore::~AppContextCore()
+{
+#ifdef TRACE
+    std::cout << __PRETTY_FUNCTION__ << std::endl;
+#endif
+}
+
+OpenURLBaseScope &AppContextCore::getOpenURLBase() const
+{
+    return App::instance().openUrlBaseScope;
+}
+
+CybergraphicsBaseScope &AppContextCore::getCybergraphicsBase() const
+{
+    return App::instance().cybergraphicsBaseScope;
+}
---
+++ app/src/AppContext.hpp
@@ -0,0 +1,28 @@
+//
+//  CPU-M
+//
+//  (c) 2025 TDolphin
+//
+
+#pragma once
+
+#include "AOS/Cybergraphics/CybergraphicsBaseScope.hpp"
+#include "AOS/OpenURL/OpenURLBaseScope.hpp"
+#include "Core/Singleton.hpp"
+
+#include <set>
+
+class AppContextCore
+{
+    friend class td::Singleton<AppContextCore>;
+
+    AppContextCore();
+    ~AppContextCore();
+
+  public:
+    OpenURLBaseScope &getOpenURLBase() const;
+    CybergraphicsBaseScope &getCybergraphicsBase() const;
+};
+
+/// @brief main AppContext (singleton) object
+typedef class td::Singleton<AppContextCore> AppContext;
