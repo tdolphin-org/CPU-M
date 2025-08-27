51b07fd5e9a98944bb0bd91e2f6189190d006d04
Rafal Zabdyr <rafal.zabdyr@tdolphin.org>
2025-08-27 19:16:18 +0200

    add Labels

---
+++ app/src/TextResources/Labels.cpp
@@ -0,0 +1,27 @@
+//
+//  CPU-M
+//
+//  (c) 2025 TDolphin
+//
+
+#include "Labels.hpp"
+
+#include "ProgDefines.hpp"
+#include "Version.hpp"
+
+#include <libraries/mui.h>
+
+namespace Label
+{
+    const char *CPUTab = "CPU/FPU";
+    const char *MainboardTab = "Mainboard";
+    const char *MemoryTab = "Memory";
+    const char *GraphicsTab = "Graphics";
+    const char *ExpansionsTab = "Expansions";
+    const char *OperatingSystemTab = "OS";
+    const char *BenchTab = "Bench";
+    const char *AboutTab = "About";
+
+    const char *AboutContent = MUIX_C MUIX_B APP_NAME "\n\n" MUIX_N "Version " APP_VERSION " " APP_DATE "\n\n" APP_DESCRIPTION
+                                                      "\n\n" MUIX_B APP_TDOLPHIN_COPYRIGHTS MUIX_N "\n\nAuthor: " AUTHORS "\n";
+}
---
+++ app/src/TextResources/Labels.hpp
@@ -0,0 +1,28 @@
+//
+//  CPU-M
+//
+//  (c) 2025 TDolphin
+//
+
+#pragma once
+
+//
+// using 'const char *' instead of const std::string &
+// for better performance (open and close of application)
+// no memory allock/free on start/close
+// important for low hardware (AmigaOS 68k)
+//
+
+namespace Label
+{
+    extern const char *CPUTab;
+    extern const char *MainboardTab;
+    extern const char *MemoryTab;
+    extern const char *GraphicsTab;
+    extern const char *ExpansionsTab;
+    extern const char *OperatingSystemTab;
+    extern const char *BenchTab;
+    extern const char *AboutTab;
+
+    extern const char *AboutContent;
+}
