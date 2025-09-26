82fd30d2f048dcf70507136c98bb81d724ae5bb6
Rafal Zabdyr <rafal.zabdyr@tdolphin.org>
2025-09-26 22:46:27 +0200

    update makefile.gen.version

--- app/makefile.gen.version.mk
+++ app/makefile.gen.version.mk
@@ -1,24 +1,26 @@
 src/Version.cpp: FORCE
 	rm -f $@
-	echo "#include \"Version.hpp\"" >> $@
-	echo "#include \"ProgDefines.hpp\"" >> $@
-	echo "#include <iostream>" >> $@
-	echo "#pragma GCC diagnostic push" >> $@
-	echo "#pragma GCC diagnostic ignored \"-Wunused-variable\"" >> $@
-	echo "static const char *VERSION = AMIGAOS_VER_PREFIX CMD_NAME \" \" APP_VERSION \" \" APP_DATE \" [\" APP_BUILD_COMMIT_HASH \"]\\\r\\\n\";" >> $@
-	echo "#pragma GCC diagnostic pop" >> $@
-	echo "const char *SCREEN_TITLE = APP_NAME \" \" APP_VERSION \" \" APP_TDOLPHIN_COPYRIGHTS;" >> $@
-	echo "using namespace std;" >> $@
-	echo "void About() { cout << APP_NAME << \" \" << APP_VERSION << \" \" << APP_DATE << \" [\" << APP_BUILD_COMMIT_HASH << \"]\" << endl \
+	@echo "generate Version.cpp"
+	@echo "#include \"Version.hpp\"" >> $@
+	@echo "#include \"ProgDefines.hpp\"" >> $@
+	@echo "#include <iostream>" >> $@
+	@echo "#pragma GCC diagnostic push" >> $@
+	@echo "#pragma GCC diagnostic ignored \"-Wunused-variable\"" >> $@
+	@echo "static const char *VERSION = AMIGAOS_VER_PREFIX CMD_NAME \" \" APP_VERSION \" \" APP_DATE \" [\" APP_BUILD_COMMIT_HASH \"]\\\r\\\n\";" >> $@
+	@echo "#pragma GCC diagnostic pop" >> $@
+	@echo "const char *SCREEN_TITLE = APP_NAME \" \" APP_VERSION \" \" APP_TDOLPHIN_COPYRIGHTS;" >> $@
+	@echo "using namespace std;" >> $@
+	@echo "void About() { cout << APP_NAME << \" \" << APP_VERSION << \" \" << APP_DATE << \" [\" << APP_BUILD_COMMIT_HASH << \"]\" << endl \
 	<< APP_TDOLPHIN_COPYRIGHTS << endl; }" >> $@
 
 src/Version.hpp: FORCE
 	rm -f $@
-	echo "#pragma once " >> $@
-	echo "#define APP_VERSION \"$(APP_VERSION)\"" >> $@
-	echo "#define APP_DATE \"($(APP_DATE))\"" >> $@
-	echo "#define APP_BUILD_COMMIT_HASH \"$(APP_BUILD_COMMIT_HASH)\"" >> $@
-	echo "extern const char *SCREEN_TITLE;" >> $@
-	echo "void About();" >> $@
+	@echo "generate Version.hpp"
+	@echo "#pragma once " >> $@
+	@echo "#define APP_VERSION \"$(APP_VERSION)\"" >> $@
+	@echo "#define APP_DATE \"($(APP_DATE))\"" >> $@
+	@echo "#define APP_BUILD_COMMIT_HASH \"$(APP_BUILD_COMMIT_HASH)\"" >> $@
+	@echo "extern const char *SCREEN_TITLE;" >> $@
+	@echo "void About();" >> $@
 
 FORCE:
