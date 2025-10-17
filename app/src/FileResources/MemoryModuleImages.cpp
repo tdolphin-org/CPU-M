6257600affebc3cdca002998486ea409df645f87
Rafal Zabdyr <rafal.zabdyr@tdolphin.org>
2025-10-17 22:07:02 +0200

    add MemoryModuleImages header and implementation files with ddr_dimm constant

---
+++ app/src/FileResources/MemoryModuleImages.cpp
@@ -0,0 +1,12 @@
+//
+//  CPU-M
+//
+//  (c) 2025 TDolphin
+//
+
+#include "MemoryModuleImages.hpp"
+
+namespace MemoryModuleImageFile
+{
+    const char *ddr_dimm = "PROGDIR:images/memory/ddr.dimm.800x256.png";
+}
---
+++ app/src/FileResources/MemoryModuleImages.hpp
@@ -0,0 +1,12 @@
+//
+//  CPU-M
+//
+//  (c) 2025 TDolphin
+//
+
+#pragma once
+
+namespace MemoryModuleImageFile
+{
+    extern const char *ddr_dimm;
+}
