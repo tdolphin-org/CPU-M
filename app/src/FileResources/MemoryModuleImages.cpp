565d8aa0e6d8f2680dde5d05e9079401d8a44194
Rafal Zabdyr <rafal.zabdyr@tdolphin.org>
2025-10-18 19:02:32 +0200

    add memory module images

--- app/src/FileResources/MemoryModuleImages.cpp
+++ app/src/FileResources/MemoryModuleImages.cpp
@@ -8,5 +8,14 @@
 
 namespace MemoryModuleImageFile
 {
-    const char *ddr_dimm = "PROGDIR:images/memory/ddr.dimm.800x256.png";
+    const char *default_dimm = "PROGDIR:images/memory/default.dimm.810x190.png";
+    const char *default_so_dimm = "PROGDIR:images/memory/default.so_dimm.410x190.png";
+    const char *sdr_dimm = "PROGDIR:images/memory/sdr.dimm.810x190.png";
+    const char *sdr_so_dimm = "PROGDIR:images/memory/sdr.so_dimm.410x190.png";
+    const char *ddr_dimm = "PROGDIR:images/memory/ddr.dimm.810x190.png";
+    const char *ddr_so_dimm = "PROGDIR:images/memory/ddr.so_dimm.410x190.png";
+    const char *ddr2_dimm = "PROGDIR:images/memory/ddr2.dimm.810x190.png";
+    const char *ddr2_so_dimm = "PROGDIR:images/memory/ddr2.so_dimm.410x190.png";
+    const char *ddr3_dimm = "PROGDIR:images/memory/ddr3.dimm.810x190.png";
+    const char *ddr3_so_dimm = "PROGDIR:images/memory/ddr3.so_dimm.410x190.png";
 }
--- app/src/FileResources/MemoryModuleImages.hpp
+++ app/src/FileResources/MemoryModuleImages.hpp
@@ -8,5 +8,14 @@
 
 namespace MemoryModuleImageFile
 {
+    extern const char *default_dimm;
+    extern const char *default_so_dimm;
+    extern const char *sdr_dimm;
+    extern const char *sdr_so_dimm;
     extern const char *ddr_dimm;
+    extern const char *ddr_so_dimm;
+    extern const char *ddr2_dimm;
+    extern const char *ddr2_so_dimm;
+    extern const char *ddr3_dimm;
+    extern const char *ddr3_so_dimm;
 }
