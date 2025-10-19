000bba6ede3eed249883519766e83f8dec2b03c5
Rafal Zabdyr <rafal.zabdyr@tdolphin.org>
2025-10-19 12:52:05 +0200

    fix: correct naming convention for SO-DIMM image files

--- app/src/FileResources/MemoryModuleImages.cpp
+++ app/src/FileResources/MemoryModuleImages.cpp
@@ -10,11 +10,11 @@ namespace MemoryModuleImageFile
 {
     const char *default_dimm = "PROGDIR:images/memory/default.dimm.810x190.png";
     const char *sdr_dimm = "PROGDIR:images/memory/sdr.dimm.810x190.png";
-    const char *sdr_so_dimm = "PROGDIR:images/memory/sdr.so_dimm.410x190.png";
+    const char *sdr_so_dimm = "PROGDIR:images/memory/sdr.so-dimm.410x190.png";
     const char *ddr_dimm = "PROGDIR:images/memory/ddr.dimm.810x190.png";
-    const char *ddr_so_dimm = "PROGDIR:images/memory/ddr.so_dimm.410x190.png";
+    const char *ddr_so_dimm = "PROGDIR:images/memory/ddr.so-dimm.410x190.png";
     const char *ddr2_dimm = "PROGDIR:images/memory/ddr2.dimm.810x190.png";
-    const char *ddr2_so_dimm = "PROGDIR:images/memory/ddr2.so_dimm.410x190.png";
+    const char *ddr2_so_dimm = "PROGDIR:images/memory/ddr2.so-dimm.410x190.png";
     const char *ddr3_dimm = "PROGDIR:images/memory/ddr3.dimm.810x190.png";
-    const char *ddr3_so_dimm = "PROGDIR:images/memory/ddr3.so_dimm.410x190.png";
+    const char *ddr3_so_dimm = "PROGDIR:images/memory/ddr3.so-dimm.410x190.png";
 }
