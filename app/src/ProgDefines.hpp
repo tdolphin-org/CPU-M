363c2f2dfc7ae4da2db772f7b326fe57d00a3e95
Rafal Zabdyr <rafal.zabdyr@tdolphin.org>
2025-10-22 22:55:56 +0200

    update progdefines

--- app/src/ProgDefines.hpp
+++ app/src/ProgDefines.hpp
@@ -15,7 +15,7 @@
 #define APP_DESCRIPTION "System Information Tool"
 #define APP_TDOLPHIN_COPYRIGHTS "(c) 2025 TDolphin"
 
-#define TDOLPHIN_WEBPAGE "http://www.tdolphin.org"
+#define TDOLPHIN_WEBPAGE "https://www.tdolphin.org"
 #define TDOLPHIN_EMAIL "info@tdolphin.org"
 #define GIT_REPOSITORY_URL "https://github.com/tdolphin-org/CPU-M"
 #define YOUTUBE_CHANNEL_URL "https://www.youtube.com/@tdolphin_org"
