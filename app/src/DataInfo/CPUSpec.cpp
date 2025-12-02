4e1337f18281a75f3cf48a01a9fa7f218bd9e334
Rafal Zabdyr <rafal.zabdyr@tdolphin.org>
2025-12-02 18:02:16 +0100

    update CPU specifications

--- app/src/DataInfo/CPUSpec.cpp
+++ app/src/DataInfo/CPUSpec.cpp
@@ -33,26 +33,34 @@ namespace DataInfo
         { "Unknown", { "??", "Unknown", "", "??", TechnologyID::Unknown, "??", "??" } },
         { "602", { "Motorola/IBM", "602", "", "3.3 V", TechnologyID::um_0_5, "February 1995", "~1.2 W" } },
         { "603", { "Motorola/IBM", "603", "", "3.3 V", TechnologyID::um_0_5, "1994", "~2.2-3.5 W" } },
-        { "603E", { "Motorola/IBM", "603e", "", "2.5 V, 3.3 V", TechnologyRange{ TechnologyID::um_0_35, TechnologyID::um_0_5 }, "April 1996", "~6 W" } },
+        { "603E",
+          { "Motorola/IBM", "603e", "", "2.5 V, 3.3 V", TechnologyRange { TechnologyID::um_0_35, TechnologyID::um_0_5 }, "April 1996",
+            "~6 W" } },
         { "603P", { "Motorola/IBM", "603p", "", "2.5 V", TechnologyID::um_0_35, "?", "~4-5 W" } },
         { "604", { "Motorola/IBM", "604", "", "3.3 V", TechnologyID::um_0_5, "April 1994", "~14-17 W" } },
         { "604E", { "Motorola/IBM", "604e", "", "2.5 V", TechnologyID::um_0_35, "1996", "~16-18 W" } },
-        { "620", { "Motorola", "620", "", "3.3 V", TechnologyRange{ TechnologyID::um_0_35, TechnologyID::um_0_5 }, "1997", "~30 W" } },
+        { "620", { "Motorola", "620", "", "3.3 V", TechnologyRange { TechnologyID::um_0_35, TechnologyID::um_0_5 }, "1997", "~30 W" } },
         { "750CX", { "Motorola/IBM", "750CX", "Sidewinder", "1.8 V", TechnologyID::um_0_18, "2000", "~3.6-10 W" } }, // pegasos I/II
         { "7400", { "Motorola/IBM", "MPC7400", "Max", "1.8 V", TechnologyID::um_0_18, "1999/2000", "~10-13 W" } }, // apple G4
         { "7410", { "Motorola/Freescale", "MPC7410", "", "2 V", TechnologyID::um_0_18, "2001", "~6-14 W" } }, // apple G4
         { "7450", { "Motorola/Freescale", "MPC7450", "", "1.9 V", TechnologyID::um_0_18, "2001", "~22-39 W" } }, // apple G4
         { "7455", { "Motorola/Freescale", "MPC7455", "", "1.3 V", TechnologyID::um_0_18, "2002", "~11-22 W" } }, // apple G4
         { "7447",
-          { "Motorola/Freescale", "MPC7447", "Apollo 7", "1.1-1.3 V", TechnologyID::nm_130, "2005", "~8-20 W" } }, // pegasos II, apple powerbook g4
+          { "Motorola/Freescale", "MPC7447", "Apollo 7", "1.1-1.3 V", TechnologyID::nm_130, "2005",
+            "~8-20 W" } }, // pegasos II, apple powerbook g4
         { "7447/7457",
-          { "Motorola/Freescale", "MPC7447/7457", "", "1.1-1.3/1.1-1.6 V", TechnologyID::nm_130, "2005", "~8-20/~15-26 W" } }, // apple powerbook g4
+          { "Motorola/Freescale", "MPC7447/7457", "", "1.1-1.3/1.1-1.6 V", TechnologyID::nm_130, "2005",
+            "~8-20/~15-26 W" } }, // apple powerbook g4
         { "7447A", { "Motorola/Freescale", "MPC7447A", "", "1.1-1.3 V", TechnologyID::nm_130, "2005", "~8-20 W" } }, // apple G4
         { "7448", { "Motorola/Freescale", "MPC7448", "", "1.0-1.3 V", TechnologyID::nm_90, "2005", "~15-21 W" } }, // apple G4
-        { "970FX", { "IBM", "970FX", "", "1.2 V", TechnologyID::nm_90, "2005", "~50 W" } }, // apple G5
-        { "970MP", { "IBM", "970MP", "Antares", "0.9-1.3 V", TechnologyID::nm_90, "2005", "~25-100 W" } }, // apple G5 dual core
+        { "970",
+          { "IBM", "970", "", "0.9-1.5 V", TechnologyRange { TechnologyID::nm_130, TechnologyID::nm_90 }, "2003",
+            "~19-150 W" } }, // apple G5 dual core (specs for 970/970+)
+        { "970FX", { "IBM", "970FX", "", "0.9-1.35 V", TechnologyID::nm_90, "2005", "~13-95 W" } }, // apple G5 dual core
+        { "970MP",
+          { "IBM", "970MP", "Antares", "0.9-1.35 V", TechnologyID::nm_90, "2005", "~10-125 W" } }, // apple G5 dual core (quad up to 275W)
         { "MPC5200B", { "Freescale", "MPC5200B", "", "1.8 V", TechnologyID::um_0_18, "2005", "~1-2 W" } }, // efika
-        { "460ex", { "AMCC", "460ex", "", "1.0 V", TechnologyID::nm_90, "2007", "~4-5 W" } }, // sam460
+        { "460EX", { "AMCC", "460ex", "", "1.0 V", TechnologyID::nm_90, "2007", "~4-5 W" } }, // sam460
         { "P3041", { "Freescale/NXP", "QorIQ P3041", "", "1.0 V", TechnologyID::nm_45, "2010", "~12 W" } }, // amigaone x5000
         { "P5020", { "Freescale/NXP", "QorIQ P5020", "", "1.0-1.1 V", TechnologyID::nm_45, "2011", "~13-16 W" } }, // amigaone x5000
         { "P5040", { "Freescale/NXP", "QorIQ P5040", "", "1.1-1.2 V", TechnologyID::nm_45, "2012", "~27-33 W" } }, // amigaone x5000
