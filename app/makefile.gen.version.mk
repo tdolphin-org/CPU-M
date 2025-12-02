c91a5afaf6deeb1fd3034ff44f807cc39055e399
Rafal Zabdyr <rafal.zabdyr@tdolphin.org>
2025-12-02 08:27:24 +0100

    Update makefile.gen.version.mk

--- app/makefile.gen.version.mk
+++ app/makefile.gen.version.mk
@@ -6,7 +6,7 @@ src/Version.cpp: FORCE
 	@echo "#include <iostream>" >> $@
 	@echo "#pragma GCC diagnostic push" >> $@
 	@echo "#pragma GCC diagnostic ignored \"-Wunused-variable\"" >> $@
-	@echo "static const char *VERSION = AMIGAOS_VER_PREFIX CMD_NAME \" \" APP_VERSION \" \" APP_DATE \" [\" APP_BUILD_COMMIT_HASH \"]\\\r\\\n\";" >> $@
+	@echo "static const char *VERSION = AMIGAOS_VER_PREFIX CMD_NAME \" \" APP_VERSION \" \" APP_DATE \" \" APP_TDOLPHIN_COPYRIGHTS \" [\" APP_BUILD_COMMIT_HASH \"]\\\r\\\n\";" >> $@
 	@echo "#pragma GCC diagnostic pop" >> $@
 	@echo "const char *SCREEN_TITLE = APP_NAME \" \" APP_VERSION \" \" APP_TDOLPHIN_COPYRIGHTS;" >> $@
 	@echo "using namespace std;" >> $@
