1eec7bff0f0669a10bf8385f4e8197155a651e99
Rafal Zabdyr <rafal.zabdyr@tdolphin.org>
2026-01-15 21:31:50 +0100

    add manufacturer logos for bplan, apple, acube, a-eon, and mirari

---
+++ app/src/FileResources/ManufacturerImages.cpp
@@ -0,0 +1,16 @@
+//
+//  CPU-M
+//
+//  (c) 2025-2026 TDolphin
+//
+
+#include "ManufacturerImages.hpp"
+
+namespace ManufacturerImageFile
+{
+    const char *bplan_logo = "PROGDIR:images/vendors/bplan.logo.64x64.png";
+    const char *apple_logo = "PROGDIR:images/vendors/apple.logo.aqua.85x104.png";
+    const char *acube_logo = "PROGDIR:images/vendors/acube.logo.100x100.png";
+    const char *a_eon_logo = "PROGDIR:images/vendors/a_eon.logo.100x100.png";
+    const char *mirari_logo = "PROGDIR:images/vendors/mirari.logo.100x100.png";
+}
