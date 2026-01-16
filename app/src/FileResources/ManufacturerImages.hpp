08c0833292877c5a889ecccf6a84a3ef904f7697
Rafal Zabdyr <rafal.zabdyr@tdolphin.org>
2026-01-16 21:39:05 +0100

    reorder manufacturer logo declarations for consistency

--- app/src/FileResources/ManufacturerImages.hpp
+++ app/src/FileResources/ManufacturerImages.hpp
@@ -8,9 +8,9 @@
 
 namespace ManufacturerImageFile
 {
-    extern const char *bplan_logo;
-    extern const char *apple_logo;
-    extern const char *acube_logo;
     extern const char *a_eon_logo;
+    extern const char *acube_logo;
+    extern const char *apple_logo;
+    extern const char *bplan_logo;
     extern const char *mirari_logo;
 }
