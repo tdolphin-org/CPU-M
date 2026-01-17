a0d712147ceeb15ef3710c03429240ca7ff1afae
Rafal Zabdyr <rafal.zabdyr@tdolphin.org>
2026-01-17 15:33:44 +0100

    fix filename for A-Eon logo in ManufacturerImages

--- app/src/FileResources/ManufacturerImages.cpp
+++ app/src/FileResources/ManufacturerImages.cpp
@@ -8,7 +8,7 @@
 
 namespace ManufacturerImageFile
 {
-    const char *a_eon_logo = "PROGDIR:images/manufacturers/a_eon.logo.100x100.png";
+    const char *a_eon_logo = "PROGDIR:images/manufacturers/a-eon.logo.100x100.png";
     const char *acube_logo = "PROGDIR:images/manufacturers/acube.logo.100x100.png";
     const char *apple_logo = "PROGDIR:images/manufacturers/apple.logo.aqua.85x104.png";
     const char *bplan_logo = "PROGDIR:images/manufacturers/bplan.logo.64x64.png";
